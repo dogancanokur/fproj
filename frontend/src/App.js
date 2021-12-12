171f3fce0f4501cd8ace7435b394a38f5abb08f1
Doğancan Okur <dogancanokur@gmail.com>
2021-12-12 05:14:42 +0300

    basic form post - proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import UserSignUpPage from "./UserSignUpPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <UserSignUpPage/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <UserSignUpPage/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
