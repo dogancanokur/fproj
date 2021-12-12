d6dc11483a962e0226f7e244fa87f4b476a03584
Doğancan Okur <dogancanokur@gmail.com>
2021-12-12 05:11:26 +0300

    basic form post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Raise and shine</h1>
+        <UserSignUpPage/>
       </header>
     </div>
   );
